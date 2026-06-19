f8b04d8589d0f75a5fcea8403be6ca2314bf08a3
雪狐咕 <17019863+snowy-fox-murmur@user.noreply.gitee.com>
2026-06-20 01:55:16 +0800

    xhg的第一个个人博客

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -6,21 +6,22 @@ const SITE_LANG = "zh_CN";
 
 export const siteConfig: SiteConfig = {
 	// 站点标题
-	title: "Firefly",
+	title: "雪狐咕的小窝",
 
 	// 站点副标题
-	subtitle: "Demo site",
+	subtitle: "xhg-zzzz",
 
 	// 站点 URL
-	site_url: "https://firefly.cuteleaf.cn",
+	site_url: "https://394000.xyz",
 
 	// 站点描述
 	description:
-		"Firefly 是一款基于 Astro 框架和 Fuwari 模板开发的清新美观且现代化个人博客主题模板，专为技术爱好者和内容创作者设计。该主题融合了现代 Web 技术栈，提供了丰富的功能模块和高度可定制的界面，让您能够轻松打造出专业且美观的个人博客网站。",
+		"xhg个人博客网站 it's my first blog! ",
+		
 
 	// 站点关键词
 	keywords: [
-		"Firefly",
+		"雪狐咕",
 		"Fuwari",
 		"Astro",
 		"ACGN",
@@ -78,7 +79,7 @@ export const siteConfig: SiteConfig = {
 			alt: "🍀",
 		},
 		// 导航栏标题
-		title: "Firefly",
+		title: "雪狐咕的小窝",
 		// 全宽导航栏，导航栏是否占满屏幕宽度
 		widthFull: false,
 		// 导航菜单对齐方式，left：左对齐，center：居中
