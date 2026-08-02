93c9e50b437d4a92d0905637a30ae830e8919ba8
xuehugu-zzzz <f1436098944@gmail.com>
2026-08-02 11:01:50 +0800

    Remove sponsor and about pages from navigation

--- src/config/navBarConfig.ts
+++ src/config/navBarConfig.ts
@@ -53,20 +53,6 @@ const getDynamicNavBarConfig = (): NavBarConfig => {
 		],
 	});
 
-	// 关于及其子菜单
-	links.push({
-		name: "关于",
-		url: "#",
-		icon: "material-symbols:info",
-		children: [
-			// 打赏
-			LinkPresets.Sponsor,
-
-			// 关于页面
-			LinkPresets.About,
-		],
-	});
-
 	// 自定义导航栏链接
 	links.push({
 		name: "链接",
